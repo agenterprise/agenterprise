74f6c56195588ea77f90d96a35c56e81ffb01422
michael from semaverse dev <mail@semaverse.com>
2025-10-08 12:46:22 +0200

    fix test

--- test/unit/model/test_project.py
+++ test/unit/model/test_project.py
@@ -4,6 +4,8 @@ def test_project_setup_default():
 
     service_techstack = AIURN("aiurn:techstack:github::agenterprise:service-layer-fastapi-base")
     ai_techstack = AIURN("aiurn:techstack:github::agenterprise:ai-layer-pydanticai")
-    p = Project(ai_techstack=ai_techstack, service_techstack=service_techstack,target_dir="/tmp",envid="12344")
+    data_techstack = AIURN("aiurn:techstack:github::agenterprise:data-layer-pydantic")
+    p = Project(ai_techstack=ai_techstack, service_techstack=service_techstack, data_techstack=data_techstack, target_dir="/tmp",envid="12344")
 
     assert p.get_projectlayer() == "https://github.com/agenterprise/service-layer-fastapi-base.git"
+    assert p.get_agentlayer() == "https://github.com/agenterprise/ai-layer-pydanticai.git"
